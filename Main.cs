34368a705c14f2a5cdc29537534d2358b5586d4a
Hadia Abid <hadiaabid98@gmail.com>
2026-05-22 19:19:50 +0500

    Add main.cs for main form

---
+++ Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarSystem
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer cr= new Customer();
+            cr.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Car car = new Car();
+            car.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Category cy = new Category();
+            cy.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Rental rl= new Rental();    
+            rl.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Payment pt= new Payment();  
+            pt.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Maintenance me= new Maintenance();  
+            me.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Dashboard dd= new Dashboard();  
+            dd.Show();
+        }
+    }
+}
