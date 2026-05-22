328ca8e5e0d1c54aad2f317871814648b48af08a
Hadia Abid <hadiaabid98@gmail.com>
2026-05-22 19:17:50 +0500

    Add form1.cs for login form

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CarSystem
 {
@@ -16,5 +17,41 @@ namespace CarSystem
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Create database connection
+            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
+            // Open database connection
+            con.Open();
+            // Store username entered by user
+            string username = txtUsername.Text;
+            // Store password entered by user
+            string password = txtPassword.Text;
+            // Create SQL query for login checking
+            SqlCommand cmd = new SqlCommand("select Username,Password from logintab where Username='" + txtUsername.Text + "'and Password='" + txtPassword.Text + "'", con);
+            // Create DataAdapter object to fetch data
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            // Create DataTable to store fetched data
+            DataTable dt = new DataTable();
+            // Fill DataTable with database data
+            da.Fill(dt);
+            // Check Wheather user exists or not
+            if (dt.Rows.Count > 0)
+            {
+                Main mn = new Main();
+                mn.Show();
+                
+            }
+            else
+            {
+                // Show error message if login fails
+                MessageBox.Show("Invalid Login");
+            }
+            // Close database connection
+            con.Close();
+
+        }
     }
+    
 }
