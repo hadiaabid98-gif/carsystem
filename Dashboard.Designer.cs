01c7c2847c3681f3ec52db463b21df2897be2523
Hadia Abid <hadiaabid98@gmail.com>
2026-05-22 19:15:15 +0500

    Add dashboard.designer.cs UI layout for dashboard form

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace CarSystem
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lblCount3 = new System.Windows.Forms.Label();
+            this.lblCat = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblCount2 = new System.Windows.Forms.Label();
+            this.lblCars = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblCount1 = new System.Windows.Forms.Label();
+            this.lblCustomer = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel5.Controls.Add(this.lblCount3);
+            this.panel5.Controls.Add(this.lblCat);
+            this.panel5.Location = new System.Drawing.Point(798, 69);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(264, 215);
+            this.panel5.TabIndex = 7;
+            // 
+            // lblCount3
+            // 
+            this.lblCount3.AutoSize = true;
+            this.lblCount3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount3.Location = new System.Drawing.Point(34, 89);
+            this.lblCount3.Name = "lblCount3";
+            this.lblCount3.Size = new System.Drawing.Size(36, 37);
+            this.lblCount3.TabIndex = 2;
+            this.lblCount3.Text = "0";
+            // 
+            // lblCat
+            // 
+            this.lblCat.AutoSize = true;
+            this.lblCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCat.Location = new System.Drawing.Point(19, 37);
+            this.lblCat.Name = "lblCat";
+            this.lblCat.Size = new System.Drawing.Size(191, 37);
+            this.lblCat.TabIndex = 1;
+            this.lblCat.Text = "Categories:";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel4.Controls.Add(this.lblCount2);
+            this.panel4.Controls.Add(this.lblCars);
+            this.panel4.Location = new System.Drawing.Point(534, 69);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(266, 215);
+            this.panel4.TabIndex = 8;
+            // 
+            // lblCount2
+            // 
+            this.lblCount2.AutoSize = true;
+            this.lblCount2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount2.Location = new System.Drawing.Point(68, 89);
+            this.lblCount2.Name = "lblCount2";
+            this.lblCount2.Size = new System.Drawing.Size(36, 37);
+            this.lblCount2.TabIndex = 2;
+            this.lblCount2.Text = "0";
+            // 
+            // lblCars
+            // 
+            this.lblCars.AutoSize = true;
+            this.lblCars.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCars.Location = new System.Drawing.Point(56, 37);
+            this.lblCars.Name = "lblCars";
+            this.lblCars.Size = new System.Drawing.Size(99, 37);
+            this.lblCars.TabIndex = 1;
+            this.lblCars.Text = "Cars:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel3.Controls.Add(this.lblCount1);
+            this.panel3.Controls.Add(this.lblCustomer);
+            this.panel3.Location = new System.Drawing.Point(282, 69);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(256, 215);
+            this.panel3.TabIndex = 6;
+            // 
+            // lblCount1
+            // 
+            this.lblCount1.AutoSize = true;
+            this.lblCount1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount1.Location = new System.Drawing.Point(26, 99);
+            this.lblCount1.Name = "lblCount1";
+            this.lblCount1.Size = new System.Drawing.Size(36, 37);
+            this.lblCount1.TabIndex = 1;
+            this.lblCount1.Text = "0";
+            // 
+            // lblCustomer
+            // 
+            this.lblCustomer.AutoSize = true;
+            this.lblCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomer.Location = new System.Drawing.Point(15, 37);
+            this.lblCustomer.Name = "lblCustomer";
+            this.lblCustomer.Size = new System.Drawing.Size(191, 37);
+            this.lblCustomer.TabIndex = 0;
+            this.lblCustomer.Text = "Customers:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(279, 636);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1068, 69);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(354, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin Dashboard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::CarSystem.Properties.Resources._10;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(276, 264);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 705);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label lblCount3;
+        private System.Windows.Forms.Label lblCat;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblCount2;
+        private System.Windows.Forms.Label lblCars;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblCount1;
+        private System.Windows.Forms.Label lblCustomer;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
