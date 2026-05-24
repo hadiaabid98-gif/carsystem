cab0ec2977f4d89c8f2529603bd769e6c55371ce
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:07:53 +0500

    Add categorymodel class for category entity

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarSystem.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryID { get; set; }
+        public string CarName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
