860e3983bb7857200c6a8643f0c52f0d1afb0b57
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:02:51 +0500

    Add dbhelper.cs for data access layer

---
+++ DAL/DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CarSystem.DAL
+{
+    public static class DBHelper
+    {
+        public static string ConnectionString =
+            @"Data Source=localhost\SQLEXPRESS;Initial Catalog=CarSystemDB;Integrated Security=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
