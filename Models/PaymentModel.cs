b32d0721179812069e54ba45fc117eca87b01415
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:09:57 +0500

    Add paymentmodel class for payment entity

---
+++ Models/PaymentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarSystem.Models
+{
+    public class PaymentModel
+    {
+        public int PaymentID { get; set; }
+        public string CustomerName { get; set; }
+        public int Amount { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
