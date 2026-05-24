5bd6b106e51ae6c99b2a6fb2c20e777fbb658b6c
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:24:56 +0500

    Update category.cs model

--- Category.cs
+++ Category.cs
@@ -1,81 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using CarSystem.BAL;
+using CarSystem.Models;
 
 namespace CarSystem
 {
     public partial class Category : Form
     {
-        public Category()
+        private CategoryBAL bal = new CategoryBAL();
+
+        public Category() { InitializeComponent(); }
+
+        private void Category_Load(object sender, EventArgs e)
         {
-            InitializeComponent();
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("insert into categories values(@categoryid,@carname,@categoryname)", con);
-            cnn.Parameters.AddWithValue("@categoryid", int.Parse(textBox1.Text));
-            cnn.Parameters.AddWithValue("@carname", textBox2.Text);
-            cnn.Parameters.AddWithValue("@categoryname", textBox3.Text);
-            cnn.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Saved Successfully");
-
+            CategoryModel c = new CategoryModel
+            {
+                CategoryID = int.Parse(textBox1.Text),
+                CarName = textBox2.Text,
+                CategoryName = textBox3.Text
+            };
+            bal.Save(c);
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("select * from categories", con);
-            SqlDataAdapter da = new SqlDataAdapter(cnn);
-            DataTable table = new DataTable();
-            da.Fill(table);
-            dataGridView1.DataSource = table;
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("update categories set carname=@carname,categoryname=@categoryname where categoryid=@categoryid", con);
-            cnn.Parameters.AddWithValue("@categoryid", int.Parse(textBox1.Text));
-            cnn.Parameters.AddWithValue("@carname", textBox2.Text);
-            cnn.Parameters.AddWithValue("@categoryname", textBox3.Text);
-            cnn.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Updated Successfully");
+            CategoryModel c = new CategoryModel
+            {
+                CategoryID = int.Parse(textBox1.Text),
+                CarName = textBox2.Text,
+                CategoryName = textBox3.Text
+            };
+            bal.Update(c);
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("delete from categories where categoryid=@categoryid", con);
-            cnn.Parameters.AddWithValue("@categoryid", int.Parse(textBox1.Text));
-            cnn.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Deleted Successfully");
-        }
-
-        private void Category_Load(object sender, EventArgs e)
-        {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("select * from categories", con);
-            SqlDataAdapter da = new SqlDataAdapter(cnn);
-            DataTable table = new DataTable();
-            da.Fill(table);
-            dataGridView1.DataSource = table;
+            bal.Delete(int.Parse(textBox1.Text));
+            dataGridView1.DataSource = bal.GetAll();
         }
     }
 }
