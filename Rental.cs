f451db27a3445b192ae2008277a7ff96a3eac55e
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 20:21:29 +0500

    Update rental.cs

--- Rental.cs
+++ Rental.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Windows.Forms;
 using CarSystem.BAL;
 using CarSystem.Models;
@@ -16,8 +17,28 @@ namespace CarSystem
             dataGridView1.DataSource = bal.GetAll();
         }
 
+        // ADD button
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.Value = DateTime.Now;
+            textBox1.Focus();
+        }
+
+        // SAVE button
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Please fill all fields.");
+                return;
+            }
+
             RentalModel r = new RentalModel
             {
                 RentalID = int.Parse(textBox1.Text),
@@ -32,13 +53,15 @@ namespace CarSystem
             dataGridView1.DataSource = bal.GetAll();
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            dataGridView1.DataSource = bal.GetAll();
-        }
-
+        // UPDATE button
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter Rental ID to update.");
+                return;
+            }
+
             RentalModel r = new RentalModel
             {
                 RentalID = int.Parse(textBox1.Text),
@@ -53,16 +76,29 @@ namespace CarSystem
             dataGridView1.DataSource = bal.GetAll();
         }
 
+        // DELETE button
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter Rental ID to delete.");
+                return;
+            }
+
             bal.Delete(int.Parse(textBox1.Text));
             dataGridView1.DataSource = bal.GetAll();
         }
 
+        // CLEAR button
         private void btnClear_Click(object sender, EventArgs e)
         {
-            textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = "";
-            textBox4.Text = ""; textBox5.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.Value = DateTime.Now;
         }
 
         private void panel1_Paint(object sender, System.Windows.Forms.PaintEventArgs e) { }
