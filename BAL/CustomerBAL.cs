ac057351a6c4274421d4dbdfb31a1d58485ae685
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 10:57:55 +0500

    Add customerbal.cs for business logic layer

---
+++ BAL/CustomerBAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using CarSystem.DAL;
+using CarSystem.Models;
+
+namespace CarSystem.BAL
+{
+    public class CustomerBAL
+    {
+        private CustomerDAL dal = new CustomerDAL();
+
+        public void Save(CustomerModel c)
+        {
+            dal.Insert(c);
+            MessageBox.Show("Record Saved Successfully");
+        }
+
+        public void Update(CustomerModel c)
+        {
+            dal.Update(c);
+            MessageBox.Show("Record Updated Successfully");
+        }
+
+        public void Delete(int customerID)
+        {
+            dal.Delete(customerID);
+            MessageBox.Show("Record Deleted Successfully");
+        }
+
+        public DataTable GetAll()
+        {
+            return dal.GetAll();
+        }
+    }
+}
+internal class CustomerBALcs
+{
+}
+
