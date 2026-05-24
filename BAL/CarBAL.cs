5095aae2a2bd064545f7221bd92a834f88a5f0bf
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 10:56:31 +0500

    Add carbal.cs for business logic layer

---
+++ BAL/CarBAL.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Windows.Forms;
+using CarSystem.DAL;
+using CarSystem.Models;
+
+namespace CarSystem.BAL
+{
+    public class CarBAL
+    {
+        private CarDAL dal = new CarDAL();
+
+        public void Save(CarModel car)
+        {
+            dal.Insert(car);
+            MessageBox.Show("Record Saved Successfully");
+        }
+
+        public void Update(CarModel car)
+        {
+            dal.Update(car);
+            MessageBox.Show("Record Updated Successfully");
+        }
+
+        public void Delete(int carID)
+        {
+            dal.Delete(carID);
+            MessageBox.Show("Record Deleted Successfully");
+        }
+
+        public DataTable GetAll()
+        {
+            return dal.GetAll();
+        }
+    }
+}
