d4dc41f0a1692d91a1faaa8d4af2c5afa302289e
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:10:26 +0500

    Add rentalmodel class for rental entity

---
+++ Models/RentalModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CarSystem.Models
+{
+    public class RentalModel
+    {
+        public int RentalID { get; set; }
+        public string CustomerName { get; set; }
+        public string CarName { get; set; }
+        public string Model { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string RentalStatus { get; set; }
+    }
+}
