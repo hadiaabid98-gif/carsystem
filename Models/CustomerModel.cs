b35b7f34e87334a7ae80225b32f6cedb139f16c0
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:08:32 +0500

    Add customermodel class for customer entity

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CarSystem.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
