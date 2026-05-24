6f00497b1df81e5c8aa62ebc3429f34876683b96
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:07:13 +0500

    Add carmodel class for car entity

---
+++ Models/CarModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CarSystem.Models
+{
+    public class CarModel
+    {
+        public int CarID { get; set; }
+        public string CarName { get; set; }
+        public string Model { get; set; }
+        public string PlateNumber { get; set; }
+        public string Color { get; set; }
+        public int DailyRate { get; set; }
+        public string Status { get; set; }
+    }
+}
