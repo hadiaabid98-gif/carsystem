016c113dd1e654861f70f22c14228d14ec8c37d7
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:09:25 +0500

    Add maintenancemodel class for maintenance entity

---
+++ Models/MaintenanceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CarSystem.Models
+{
+    public class MaintenanceModel
+    {
+        public int MaintenanceID { get; set; }
+        public string CarName { get; set; }
+        public string Model { get; set; }
+        public string Service { get; set; }
+        public int Cost { get; set; }
+    }
+}
