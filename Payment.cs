c31b105708c894deec50e9eedd3e3640ddbaddac
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:31:13 +0500

    Update payment.cs model

--- Payment.cs
+++ Payment.cs
@@ -1,90 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using CarSystem.BAL;
+using CarSystem.Models;
 
 namespace CarSystem
 {
     public partial class Payment : Form
     {
-        public Payment()
+        private PaymentBAL bal = new PaymentBAL();
+
+        public Payment() { InitializeComponent(); }
+
+        private void Payment_Load(object sender, EventArgs e)
         {
-            InitializeComponent();
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("insert into payments values(@paymentid,@customername,@amount,@paymentmethod)", con);
-            cnn.Parameters.AddWithValue("@paymentid", int.Parse(textBox1.Text));
-            cnn.Parameters.AddWithValue("@customerName", textBox2.Text);
-            cnn.Parameters.AddWithValue("@amount", int.Parse(textBox3.Text));
-            cnn.Parameters.AddWithValue("@paymentmethod", textBox4.Text);
-            cnn.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Saved Successfully");
+            PaymentModel p = new PaymentModel
+            {
+                PaymentID = int.Parse(textBox1.Text),
+                CustomerName = textBox2.Text,
+                Amount = int.Parse(textBox3.Text),
+                PaymentMethod = textBox4.Text
+            };
+            bal.Save(p);
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("select * from payments", con);
-            SqlDataAdapter da = new SqlDataAdapter(cnn);
-            DataTable table = new DataTable();
-            da.Fill(table);
-            dataGridView1.DataSource = table;
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("update payments set customername=@customername,amount=@amount,paymentmethod=@paymentmethod where paymentid=@paymentid", con);
-            cnn.Parameters.AddWithValue("@paymentid", int.Parse(textBox1.Text));
-            cnn.Parameters.AddWithValue("@customerName", textBox2.Text);
-            cnn.Parameters.AddWithValue("@amount", int.Parse(textBox3.Text));
-            cnn.Parameters.AddWithValue("@paymentmethod", textBox4.Text);
-            cnn.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Updated Successfully");
+            PaymentModel p = new PaymentModel
+            {
+                PaymentID = int.Parse(textBox1.Text),
+                CustomerName = textBox2.Text,
+                Amount = int.Parse(textBox3.Text),
+                PaymentMethod = textBox4.Text
+            };
+            bal.Update(p);
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("delete from payments where paymentid=@paymentid", con);
-            cnn.Parameters.AddWithValue("@paymentid", int.Parse(textBox1.Text));
-            cnn.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Record Deleted Successfully");
+            bal.Delete(int.Parse(textBox1.Text));
+            dataGridView1.DataSource = bal.GetAll();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "";
-            textBox2.Text = "";
-            textBox3.Text = "";
-            textBox4.Text = "";
-        }
-
-        private void Payment_Load(object sender, EventArgs e)
-        {
-            SqlConnection con = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CarSystemDB; Integrated Security = True; TrustServerCertificate = True");
-            con.Open();
-            SqlCommand cnn = new SqlCommand("select * from payments", con);
-            SqlDataAdapter da = new SqlDataAdapter(cnn);
-            DataTable table = new DataTable();
-            da.Fill(table);
-            dataGridView1.DataSource = table;
+            textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = ""; textBox4.Text = "";
         }
     }
 }
