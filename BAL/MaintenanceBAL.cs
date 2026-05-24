4944d8a51e81ddc0ecdade5935a7a11efa35df86
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 10:59:00 +0500

    Add maintenance bal.cs for business logic layer

---
+++ BAL/MaintenanceBAL.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Windows.Forms;
+using CarSystem.DAL;
+using CarSystem.Models;
+
+namespace CarSystem.BAL
+{
+    public class MaintenanceBAL
+    {
+        private MaintenanceDAL dal = new MaintenanceDAL();
+
+        public void Save(MaintenanceModel m)
+        {
+            dal.Insert(m);
+            MessageBox.Show("Record Saved Successfully");
+        }
+
+        public void Update(MaintenanceModel m)
+        {
+            dal.Update(m);
+            MessageBox.Show("Record Updated Successfully");
+        }
+
+        public void Delete(int maintenanceID)
+        {
+            dal.Delete(maintenanceID);
+            MessageBox.Show("Record Deleted Successfully");
+        }
+
+        public DataTable GetAll()
+        {
+            return dal.GetAll();
+        }
+    }
+}
