b73bf97b90f02d9eb1674f0baa50d30dbc387150
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 20:21:01 +0500

    Update payment.cs

--- Payment.cs
+++ Payment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Windows.Forms;
 using CarSystem.BAL;
 using CarSystem.Models;
@@ -16,8 +17,25 @@ namespace CarSystem
             dataGridView1.DataSource = bal.GetAll();
         }
 
+        // ADD button
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Focus();
+        }
+
+        // SAVE button
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Please fill all fields.");
+                return;
+            }
+
             PaymentModel p = new PaymentModel
             {
                 PaymentID = int.Parse(textBox1.Text),
@@ -29,13 +47,15 @@ namespace CarSystem
             dataGridView1.DataSource = bal.GetAll();
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            dataGridView1.DataSource = bal.GetAll();
-        }
-
+        // UPDATE button
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter Payment ID to update.");
+                return;
+            }
+
             PaymentModel p = new PaymentModel
             {
                 PaymentID = int.Parse(textBox1.Text),
@@ -47,15 +67,26 @@ namespace CarSystem
             dataGridView1.DataSource = bal.GetAll();
         }
 
+        // DELETE button
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter Payment ID to delete.");
+                return;
+            }
+
             bal.Delete(int.Parse(textBox1.Text));
             dataGridView1.DataSource = bal.GetAll();
         }
 
+        // button1 = CLEAR
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = ""; textBox4.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
         }
     }
 }
