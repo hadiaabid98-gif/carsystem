dd7bb9c200ce89eebd63a4741b2411837520593a
Hadia Abid <hadiaabid98@gmail.com>
2026-05-24 11:04:40 +0500

    Add paymentdal.cs for data access layer

---
+++ DAL/PaymentDAL.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using CarSystem.Models;
+
+namespace CarSystem.DAL
+{
+    public class PaymentDAL
+    {
+        public void Insert(PaymentModel p)
+        {
+            using (SqlConnection con = DBHelper.GetConnection())
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO payments VALUES(@paymentid,@customername,@amount,@paymentmethod)", con);
+                cmd.Parameters.AddWithValue("@paymentid", p.PaymentID);
+                cmd.Parameters.AddWithValue("@customername", p.CustomerName);
+                cmd.Parameters.AddWithValue("@amount", p.Amount);
+                cmd.Parameters.AddWithValue("@paymentmethod", p.PaymentMethod);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(PaymentModel p)
+        {
+            using (SqlConnection con = DBHelper.GetConnection())
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "UPDATE payments SET customername=@customername,amount=@amount,paymentmethod=@paymentmethod WHERE paymentid=@paymentid", con);
+                cmd.Parameters.AddWithValue("@paymentid", p.PaymentID);
+                cmd.Parameters.AddWithValue("@customername", p.CustomerName);
+                cmd.Parameters.AddWithValue("@amount", p.Amount);
+                cmd.Parameters.AddWithValue("@paymentmethod", p.PaymentMethod);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int paymentID)
+        {
+            using (SqlConnection con = DBHelper.GetConnection())
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM payments WHERE paymentid=@paymentid", con);
+                cmd.Parameters.AddWithValue("@paymentid", paymentID);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public DataTable GetAll()
+        {
+            using (SqlConnection con = DBHelper.GetConnection())
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM payments", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
